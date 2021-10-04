5a2c78bc385af620e03deaabfd4c14553a6bc033
Eyal Toledano <eyal.toledano@zebra-med.com>
2021-10-04 11:55:15 +0300

    updated version with api and swagger

---
+++ service/api.py
@@ -0,0 +1,88 @@
+import json
+import os
+import time
+from flask_restx import Api, Resource
+from flask_restx import reqparse
+from pika.spec import Channel
+from werkzeug.datastructures import FileStorage
+import pika 
+
+BACKOFF = 3
+SLEEP_TIMEOUT = 2
+
+api = Api()
+parser = api.parser()
+parser.add_argument('files', required=True, action='append')
+parser.add_argument('host', required=False)
+parser.add_argument('port', required=False, type=int)
+parser.add_argument('aet', required=False)
+parser.add_argument('aec', required=False)
+
+
+@api.route("/pss")
+@api.expect(parser)
+class MsgApi(Resource):
+    def _build_msg(self, args):
+        msg = {
+            "filesReferences": {
+                "files": []
+            }
+        }
+        files = args['files']
+        if 'port' in args: msg['port']= args['port']
+        if 'host' in args: msg['host']= args['host'] 
+        if 'aet' in args: msg['aet']= args['aet'] 
+        if 'aec' in args: msg['aec']= args['aec'] 
+        
+        for file in files:
+            if os.path.exists(file):
+                msg["filesReferences"]["files"].append({"path":file})
+            else:
+                raise FileNotFoundError(file)
+
+        return msg
+
+    def _send_msg(self):
+        args = parser.parse_args()
+        try:
+            msg = self._build_msg(args)
+            attempts = 0
+            while attempts < BACKOFF:
+                try:
+                    channel.basic_publish(exchange='', routing_key=conf.rabbit.queue, body=json.dumps(msg))
+                    break
+                except (pika.exceptions.ChannelWrongStateError,
+                        pika.exceptions.StreamLostError,
+                        pika.exceptions.ChannelClosed):
+                    time.sleep(SLEEP_TIMEOUT)
+                    rabbitmq_setup()
+                    attempts +=1
+        # log attempts
+        except FileNotFoundError as e:
+            return {"error-message": "FileNotFoundError: {}".format(str(e))}, 404
+        else:
+            return {
+                "status": "Success",
+                "msg": msg
+            },202
+
+    def get(self):
+        return self._send_msg()
+
+    def post(self):
+        return self._send_msg()
+
+
+def rabbitmq_setup():
+    global channel
+    connection = pika.BlockingConnection(pika.ConnectionParameters(conf.rabbit.host))
+    channel = connection.channel()
+    channel.queue_declare(queue=conf.rabbit.queue)
+    return channel
+
+def init_api(config):
+    global channel
+    global conf
+    conf = config
+    channel = rabbitmq_setup()
+    return api 
--- service/main.py
+++ service/main.py
@@ -3,9 +3,19 @@ import pika
 import logging
 import munch
 import json
+from api import init_api
+from flask import Flask, request, jsonify
+from threading import Thread
 from cli.CliWrapper import Cli
+from prometheus_client import start_http_server, Summary, Counter
 
 default_config = {
+    "api": {
+        "port": 8001
+    },
+    "monitor": {
+        "port": 5080
+    },
     "log" : {
         "name": "pacs_store_service",
         "level": "DEBUG"
@@ -24,20 +34,36 @@ default_config = {
 
 conf = munch.munchify(default_config)
 logger = logging.getLogger()
+REQUEST_TIME = Summary('request_processing_seconds', 'Time spent processing request')
+c_success = Counter('sent_success_counter', 'Success sending counter')
+c_failurs = Counter('sent_failurs_counter', 'Failur sending counter')
+c_msgcount = Counter('message_counter', 'Service overall requests counter')
 
+app = Flask(__name__)
+
+@REQUEST_TIME.time()
 def handle_message(ch, method, properties, msgbody):
+    """
+    handle a rabbit message with files to send though the queue
+    """
+    c_msgcount.inc()
     msg = munch.munchify(json.loads(msgbody))
-    #
     # msg override configuration options of pacs details 
-    pacs_host = msg.pacs.host if 'pacs' in msg.keys() else conf.pacs.host
-    pacs_port = msg.pacs.port if 'pacs' in msg.keys() else conf.pacs.port
-    pacs_aetitle = msg.pacs.aet if 'pacs' in msg.keys() else conf.pacs.aet
-    my_aetitle = msg.pacs.aec if 'pacs' in msg.keys() else conf.pacs.aec
-
+    pacs_host = msg.host if 'host' in msg.keys() and msg.host else conf.pacs.host
+    pacs_port = msg.port if 'port' in msg.keys() and msg.port else conf.pacs.port
+    pacs_aetitle = msg.aet if 'aet' in msg.keys() and msg.aet else conf.pacs.aet
+    my_aetitle = msg.aec if 'aec' in msg.keys() and msg.aec else conf.pacs.aec
+    # try sending
     for dicom_path in msg.filesReferences.files:
-        logger.info(f"path to store is:{dicom_path.path}")
-        store_path_to_pacs(pacs_host,pacs_port,pacs_aetitle, my_aetitle, dicom_path.path)
-
+        try:
+            store_path_to_pacs(pacs_host,pacs_port,pacs_aetitle, my_aetitle, dicom_path.path)
+            logger.info(f"successfully stored :{dicom_path.path}")
+        except ConnectionError as e:
+            logger.info(f"unable to store dicom {dicom_path.path}")
+            logger.warn(f"{str(e)}: error connecting to pacs host:{pacs_host} port {pacs_port}")
+            c_failurs.inc()
+        else:
+            c_success.inc()
 
 
 def store_path_to_pacs(pacs_host, pacs_port, pacs_aetitle, my_aetitle, dicom_path ):
@@ -54,25 +80,44 @@ def store_path_to_pacs(pacs_host, pacs_port, pacs_aetitle, my_aetitle, dicom_pat
         logger.error(f"rc: {rc} error pushing path {dicom_path} with message {error}")
         raise(ConnectionError(f"pacs rc:{rc} host:{pacs_host} port:{pacs_port}  msg:{error}"))
 
-def main():
-    global conf 
-    global logger
-    conf_filename = os.environ.get('PSSCONFIG') if 'PSSCONFIG' in os.environ else ".pss_conf"
-    config = json.load(open(conf_filename)) if os.path.isfile(conf_filename) else default_config
-    conf = munch.munchify(config)
-
-    level = logging.DEBUG if conf.log.level == "DEBUG" else logging.INFO
-    logging.basicConfig(level=level, format='%(levelname)s: %(name)s: %(asctime)s %(message)s')
-    logger = logging.getLogger(conf.log.name)
-
+def rabbitmq_setup():
     connection = pika.BlockingConnection(pika.ConnectionParameters(conf.rabbit.host))
     channel = connection.channel()
     channel.queue_declare(queue=conf.rabbit.queue)
+    return channel
 
+def rabbitmq_start(channel):
     channel.basic_consume(queue=conf.rabbit.queue, on_message_callback=handle_message, auto_ack=True)
-
     logging.info("starting to consume messages ... ")
     channel.start_consuming()
 
+def start_monitor_server():
+    start_http_server(conf.monitor.port)
+
+def start_rabbitmq_consumer_thread(channel):
+    t = Thread(target=rabbitmq_start,args=(channel,))
+    t.start()
+
+def main():
+    # global process tools:
+    # monitor   
+    start_monitor_server()
+    # messaging consumer
+    channel = rabbitmq_setup()
+    start_rabbitmq_consumer_thread(channel)
+    # service api handler
+    api = init_api(conf)
+    api.init_app(app)
+    app.run(port=conf.api.port)
+
 if __name__ == "__main__":
+    # initialize conf
+    conf_filename = os.environ.get('PSSCONFIG') if 'PSSCONFIG' in os.environ else ".pss_conf"
+    config = json.load(open(conf_filename)) if os.path.isfile(conf_filename) else default_config
+    conf = munch.munchify(config)
+    # initialize log
+    level = logging.DEBUG if conf.log.level == "DEBUG" else logging.INFO
+    logging.basicConfig(level=level, format='%(levelname)s: %(name)s: %(asctime)s %(message)s')
+    logger = logging.getLogger(conf.log.name)
+    # run everything
     main()
